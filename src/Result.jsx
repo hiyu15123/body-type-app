57cc613c04def5cd54e7c224b3be24e11108ab50
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-10-25 01:12:56 +0900

    ✨ リザルトコンポーネントを作成しました。

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,8 +1,17 @@
-export default function Result() {
+export default function Result(data, onReset) {
    return (
       <div>
+         <h2>あなたの情報</h2>
+         <p>性別：{data.gender}</p>
+         <p>年齢：{data.age}歳</p>
+         <p>身長：{data.height} cm</p>
+         <p>体重：{data.weight} kg</p>
+         <p>運動頻度：{data.activity}</p>
          <h2>診断結果</h2>
-         
+         <p>BMI：{data.bmi.toFixed(1)}</p>
+         <p>体型タイプ：{data.type}</p>
+         <p>アドバイス：{data.advice}</p>
+         <button onClick={onReset}>もう一度診断する</button>
       </div>
    )
 }
