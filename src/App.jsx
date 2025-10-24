59f206542f506c3aafe13e23602c9d6ec2c47528
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-10-24 23:37:25 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Form from './Form'
+import Result from './Result'
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>体系診断アプリ</h1>
+      <Form />
+      <Result />
+    </div>
+  )
+}
